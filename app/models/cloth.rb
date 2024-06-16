6d4438b249e88a6cc47f53083dac3c1bae06641c
agc15000 <no1basketballplayer15@gmail.com>
2024-06-16 23:56:17 +0900

    画像投稿機能ローカル実装

--- app/models/cloth.rb
+++ app/models/cloth.rb
@@ -2,6 +2,7 @@ class Cloth < ApplicationRecord
   belongs_to :user
   has_many :episodes, dependent: :destroy
   acts_as_taggable_on :tags
+  mount_uploader :image, ImageUploader
 
   def self.ransackable_attributes(auth_object = nil)
     ["created_at", "description", "id", "image", "name", "updated_at", "user_id"]
